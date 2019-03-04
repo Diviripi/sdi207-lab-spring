dda5c9ed18c51c874f441c6319b1a003dcccb6f4
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-03-04 10:30:21 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

--- sdi207-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi207-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -1,15 +1,21 @@
 package com.uniovi.tests;
 
-import static org.junit.Assert.*;
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
 
 public class NotaneitorTests {
 
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas)):
-	static String PathFirefox64 = "C:\\Program Files\\MozillaFirefox\\firefox.exe";
+	static String PathFirefox64 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	static String Geckdriver022 = "C:\\Users\\diviripi\\Desktop\\SDI\\lab 5\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
 	// En MACOSX (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas):
@@ -51,8 +57,31 @@ public class NotaneitorTests {
 		driver.quit();
 	}
 
+	// PR01. Acceder a la página principal /
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
 	}
 }
